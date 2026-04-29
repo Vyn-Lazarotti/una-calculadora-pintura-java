23b9ed87b314136da792ca56a24745acf709703c
Vinicius Lazaroti <viniciuslazarotti@gmail.com>
2026-04-29 10:43:33 -0300

    Update and rename CalculadoraPintura4261213458.java to CalculadoraPintura.java

--- CalculadoraPintura.java
+++ CalculadoraPintura.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class CalculadoraPintura4261213458 {
+public class CalculadoraPintura {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
 
